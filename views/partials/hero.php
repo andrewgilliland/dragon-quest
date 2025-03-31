70ad902f3e7fceadbb685c75550072222e66162b
Andrew Gilliland <andrewpgilliland@gmail.com>
2025-03-30 21:09:21 -0500

    feat: updates hero styles

--- views/partials/hero.php
+++ views/partials/hero.php
@@ -1,7 +1,7 @@
 <section class="bg-gray-950 text-white py-20 border-t border-b border-white/10 h-screen">
-    <div class="max-w-5xl mx-auto text-center">
-        <h1 class="text-4xl font-bold mb-4"><?= $heading ?></h1>
-        <p class="text-lg mb-8"><?= $subheading ?></p>
-        <a href="/classes.php" class="bg-indigo-600/10 hover:bg-indigo-600/25 text-indigo-500 font-light py-2 px-4 transition border border-white/10">Start Your Adventure</a>
+    <div class="flex flex-col justify-start items-start max-w-5xl mx-auto">
+        <h1 class="border border-white/10 text-3xl font-medium tracking-tight mb-4"><?= $heading ?></h1>
+        <p class="border border-white/10 text-base/7 text-gray-300 mb-8"><?= $subheading ?></p>
+        <a href="/classes.php" class="bg-indigo-600/[.05] hover:bg-indigo-600/15 text-indigo-500 font-light py-2 px-4 transition border border-white/10">Start Your Adventure</a>
     </div>
 </section>
