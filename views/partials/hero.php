418c574871dc604d38560e4a2e970ba2c37c1efe
Andrew Gilliland <andrewpgilliland@gmail.com>
2025-03-30 22:45:35 -0500

    feat: updating main heading

--- views/partials/hero.php
+++ views/partials/hero.php
@@ -1,6 +1,6 @@
 <section class="bg-gray-950 text-white py-20 border-t border-b border-white/10 h-screen">
     <div class="flex flex-col justify-start items-start max-w-5xl mx-auto">
-        <h1 class="border border-white/10 text-3xl font-medium tracking-tight mb-4"><?= $heading ?></h1>
+        <h1 class="border border-white/10 text-6xl font-light tracking-tight mb-4"><?= $heading ?></h1>
         <p class="border border-white/10 text-base/7 text-gray-300 mb-8"><?= $subheading ?></p>
         <a href="/classes" class="bg-indigo-600/[.05] hover:bg-indigo-600/15 text-indigo-500 font-light py-2 px-4 transition border border-white/10">Start Your Adventure</a>
     </div>
