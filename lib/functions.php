e79a64429bb8207c2325b03e5da1bd91fe3744fd
Andrew Gilliland <andrewpgilliland@gmail.com>
2025-04-01 20:38:37 -0500

    feat: updates functions

--- lib/functions.php
+++ lib/functions.php
@@ -1,12 +1,12 @@
 <?php
 
-function dataDump($value) {
+function dataDump(int|string|array|null $value): void {
     echo "<pre>";
     var_dump($value);
     echo "</pre>";
     die();
 }
 
-function urlIs($url) {
+function urlIs($url): string {
     return $_SERVER["REQUEST_URI"] == $url;
 }
